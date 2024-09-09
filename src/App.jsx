d4368a47838c57dd4ddbe3577e2faaa68b061539
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2024-09-09 16:55:19 +0530

    basic changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Navbar from './components/Header'
 import Selection from './components/Selection'
 import DonorSignup from './components/DonorSignup'
 import RecipientSignup from './components/RecipientSignup'
+import About from './components/About'
+import Home from './components/Home'
 
 
 const App = () => {
@@ -14,6 +16,8 @@ const App = () => {
         <Route path={'/selection'} element={<Selection/>}/>
         <Route path="/signup/donor" element={<DonorSignup />} />
         <Route path="/signup/recipient" element={<RecipientSignup />} />
+        <Route path='/about' element={<About/>}/>
+        <Route path='/home' element = {<Home/>}/>
       </Routes>
     </Router>
   )
