bb35cc622a3473c30876112eee0545c00a7951ee
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2024-09-11 15:38:31 +0530

    theme changes

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,11 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import {ChakraProvider} from "@chakra-ui/react"
 import './index.css'
+import theme from './utils/theme.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
     <App />
     </ChakraProvider>
   </StrictMode>,
